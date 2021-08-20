e52ee100ac572b32525bd9cd7c4e7526f60989d2
Andrey Kuravlev <sector1200@gmail.com>
2021-08-20 23:00:17 +0500

    remove 'test'

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ let innerHex = document.createElement('div');
 outerHex.classList.add('hexagon');
 innerHex.classList.add('hexagon');
 innerHex.classList.add('inner');
-// element.innerHTML = "Test";
 
 document.body.append(outerHex);
 outerHex.append(innerHex);
