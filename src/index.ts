8ed451b13b0dcb79644be4185c33776224f98c14
Andrey Kuravlev <sector1200@gmail.com>
2021-08-22 00:00:00 +0500

    Two animated hexs

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,34 @@
 import "./style.css";
 
-let container = document.createElement('div');
-let outerHex = document.createElement('div');
-let innerHex = document.createElement('div');
-
-container.classList.add('container');
-outerHex.classList.add('hexagon');
-outerHex.classList.add('outer');
-innerHex.classList.add('hexagon');
-innerHex.classList.add('inner');
-
+let container = document.createElement("div");
+container.classList.add("container");
 document.body.append(container);
-container.append(outerHex);
-outerHex.append(innerHex);
+
+let body = document.body;
+
+let i: number = 0;
+let marginTop = 3;
+
+// let hex = document.createElement('div');
+//     hex.classList.add('hexagon');
+//     container.append(hex);
+
+let timerId = setInterval(createHex, 1000);
+
+setTimeout(() => clearInterval(timerId), 2000);
+
+function createHex(): void {
+  let hex = document.createElement("div");
+  hex.classList.add("hexagon");
+
+  console.log("i = ", i);
+  console.log("marginTop =", marginTop);
+  hex.style.marginTop = marginTop + "px";
+  
+  console.log(hex.style.marginTop);
+  container.append(hex);
+  if (i > 0) hex.style.marginLeft = -hex.clientWidth / 2 + "px";
+  console.log("hex height = ", hex.clientHeight);
+  i++;
+  marginTop += hex.clientHeight * i + hex.clientWidth / (1.73 * 2) + 3;
+}
