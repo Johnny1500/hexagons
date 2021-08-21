f1db4468a7cd6f45ef288468ec8e5bbe74d1c589
Andrey Kuravlev <sector1200@gmail.com>
2021-08-21 21:21:16 +0500

    animated hexagon

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-import './style.css';
+import "./style.css";
 
+let container = document.createElement('div');
 let outerHex = document.createElement('div');
 let innerHex = document.createElement('div');
 
+container.classList.add('container');
 outerHex.classList.add('hexagon');
+outerHex.classList.add('outer');
 innerHex.classList.add('hexagon');
 innerHex.classList.add('inner');
 
-document.body.append(outerHex);
+document.body.append(container);
+container.append(outerHex);
 outerHex.append(innerHex);
